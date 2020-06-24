2df07200e20f5c292171c1b726f8ebf97bbf1947
ashlieghdawn <61922862+ashlieghdawn@users.noreply.github.com>
2020-06-23 22:21:15 -0700

    update add files

--- mainapp/mainapp/views.py
+++ mainapp/mainapp/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "home.html", {})
+    products ={"Cherries","Apples","Oranges", "Strawberries","Pears", "Watermelons"}
+    context = {
+        'products' : products,
+    }
+    return render(request, "home.html", context)
